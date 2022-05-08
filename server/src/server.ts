3912728a7efc2a98996f5a7b75fa1df20b3775f4
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-05-08 15:42:15 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
+import { ADDRGETNETWORKPARAMS } from 'dns';
 
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
